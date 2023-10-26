c5b5582c110b4bded28292b6c4e21876bdb65be1
FA <fabricecoder009@gmail.com>
2023-10-26 16:22:44 +0200

    Customizing styles in header file

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
     }
   }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-slate-200 shadow-md stroke-lime-600'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
